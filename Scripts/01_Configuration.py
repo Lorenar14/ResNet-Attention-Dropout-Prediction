f2433ba9e6165962aa59f33fbd4eee88ca4d1329
Lorenar14 <loren_asprilla74221@elpoli.edu.co>
2026-07-27 17:37:33 -0500

    Update 01_Configuration.py

--- Scripts/01_Configuration.py
+++ Scripts/01_Configuration.py
@@ -1,1 +1,21 @@
+# =============================================================================
+# EARLY WARNING SYSTEM FOR UNIVERSITY DROPOUT PREDICTION
+# Environment Configuration
+# =============================================================================
 
+%%capture
+
+# -----------------------------------------------------------------------------
+# Install required Python packages (Google Colab)
+# -----------------------------------------------------------------------------
+
+!pip install -q lifelines
+!pip install -q graphviz
+!pip install -q openpyxl
+
+# -----------------------------------------------------------------------------
+# Install Microsoft TrueType fonts (Optional)
+# -----------------------------------------------------------------------------
+
+!apt-get -qq install ttf-mscorefonts-installer
+!fc-cache -fv
